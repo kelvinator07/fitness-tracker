c1f88f8ed6fe5de9fb93ab8553f15f259aa8b3d2
isievwore <isievwore@systemspecs.com.ng>
2020-07-30 14:16:50 +0100

    updated model, views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
+  get 'welcome/about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
