a5588efc864a8f3cd05ccf989aac035294ce1f64
isievwore <isievwore@systemspecs.com.ng>
2020-08-08 14:02:45 +0100

    Updated Nav Bar

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -9,3 +9,21 @@ $(function() {
 $('.card').on('click', function(event) {
     alert('You clicked the Bootstrap Card');
 });
+
+var header = document.querySelectorAll("nav-link");
+console.log('header> ' , header)
+
+header.forEach( el => {
+  el.addEventListener('click', function() {
+    console.log('clicked here ')
+  })
+})
+
+for (var i = 0; i < header.length; i++) {
+  header[i].addEventListener("click", function() {
+    console.log('clicked here ')
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
